fcd21c3591e367a50aec4417d8d1637168bc95f0
Youchen Shen <co822ee@gmail.com>
2021-01-20 13:54:02 +0100

    call predictor names

---
+++ scr/fun_call_predictor.R
@@ -0,0 +1,23 @@
+pred <- list(c('alt10_enh','Xcoord', 'Ycoord', 'XY', 'pop2011'),
+             'clc10',
+             'clc14',
+             'clc3',
+             'clc5',
+             'clc7',
+             'MAJRDS_EU',
+             'ROADS_EU',
+             c('no2_10MACC', 'PM25_10_MACC','no2_10SAT', 'PM25_10_SAT', 'DEHM_NO2_2010'),
+             'RES')
+pred_c <- c(c('alt10_enh','Xcoord', 'Ycoord', 'XY', 'pop2011'),
+            'clc10',
+            'clc14',
+            'clc3',
+            'clc5',
+            'clc7',
+            'MAJRDS_EU',
+            'ROADS_EU',
+            c('no2_10MACC', 'PM25_10_MACC','no2_10SAT', 'PM25_10_SAT', 'DEHM_NO2_2010'),
+            'RES')
+neg_pred <- c('alt10_enh', 'clc14', 'clc7')
+station_info <- c('airid', 'Year', 'country_is', 'type_of_st', 'Xcoord', 'Ycoord', 
+                  'REGION', 'strata_run5', 'country_full')
