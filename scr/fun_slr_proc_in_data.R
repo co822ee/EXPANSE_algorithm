75c07174cdb2ac25b1a40c7e8002a59c726e5b0b
Youchen Shen <co822ee@gmail.com>
2021-01-21 11:16:15 +0100

    change truncated x, y names

--- scr/fun_slr_proc_in_data.R
+++ scr/fun_slr_proc_in_data.R
@@ -1,7 +1,7 @@
 proc_in_data <- function(in_data, neg_pred){
    # truncate x and y
-   in_data <- in_data %>% mutate(x = Xcoord-min(Xcoord)/(max(Xcoord)-min(Xcoord)),
-                                 y = Ycoord-min(Ycoord)/(max(Ycoord)-min(Ycoord)))
+   in_data <- in_data %>% mutate(x_trun = Xcoord-min(Xcoord)/(max(Xcoord)-min(Xcoord)),
+                                 y_trun = Ycoord-min(Ycoord)/(max(Ycoord)-min(Ycoord)))
    # transform altitude
    in_data <- in_data %>% mutate(alt_t = sqrt((alt10_enh-min(alt10_enh))/max(alt10_enh-min(alt10_enh))))
    # change direction of effect
