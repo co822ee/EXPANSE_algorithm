7dc295e1a81387cd855a787bbbe95112375a3170
Youchen Shen <co822ee@gmail.com>
2021-01-21 11:16:05 +0100

    plot gwr coef maps

---
+++ scr/fun_plot_gwr.R
@@ -0,0 +1,15 @@
+image(gwr.res.t$SDF,'no2_10MACC', main="no2_10MACC")
+# image(gwr.res.t$SDF,'Intercept')
+plot(eu_bnd[1],add=TRUE,pch=16,col='transparent',border='grey', alpha=0.05)
+contour(gwr.res.t$SDF,'no2_10MACC',lwd=2, labcex=1.1,add=TRUE)
+par(mfrow=c(3,4))
+plot_gwr_coef <- function(i){
+   image(gwr.res.t$SDF,names(gwr.res.t$SDF)[i], main=names(gwr.res.t$SDF)[i])
+   # image(gwr.res.t$SDF,'Intercept')
+   plot(eu_bnd[1],add=TRUE,pch=16,col='transparent',border='grey', alpha=0.05)
+   contour(gwr.res.t$SDF,names(gwr.res.t$SDF)[i],lwd=1, labcex=1.1,add=TRUE)
+}
+lapply(seq_along(names(gwr.res.t$SDF)), plot_gwr_coef)
+# method 2
+coef_stack <- stack(gwr.res.t$SDF)
+plot(coef_stack)
