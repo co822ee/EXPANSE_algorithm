73cb92ef5c00f43af07e8d9618afc3a7862ebd0d
Youchen Shen <co822ee@gmail.com>
2021-01-21 10:06:49 +0100

    generate model prediction

---
+++ scr/fun_gen_pred_df.R
@@ -0,0 +1,11 @@
+gen_pred_df <- function(model, df, obs_varname){
+   # model is the final linear regression model 
+   # df is the df in which predictor values and observations are stored (as well as the station info)
+   # This function return the observations (obs) and predictions (pred) as well as station info.
+   poll <- data.frame(pred = predict(model, df) %>% as.numeric(),
+                      obs = df[, obs_varname]) %>% 
+      mutate(res = obs - pred) %>% 
+      cbind(df %>% dplyr::select(-all_of(obs_varname)))
+   
+   return(poll)
+}
