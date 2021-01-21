99f291da3883f905c49104e6700ed4f469969426
Youchen Shen <co822ee@gmail.com>
2021-01-21 15:43:02 +0100

    run the optimal RF and return predictions and VI

---
+++ scr/fun_opt_rf.R
@@ -0,0 +1,43 @@
+opt_rf <- function(df_train, df_test, y_varname, x_varname, csv_name, 
+                   hyper_grid, seed=123){
+   
+   mtry <- hyper_grid[which.min(hyper_grid$OOB_RMSE),]$mtry
+   ntrees <- hyper_grid[which.min(hyper_grid$OOB_RMSE),]$ntrees
+   
+   eq <- as.formula(paste(y_varname, "~", paste(x_varname,collapse='+'), sep = ""))
+   rf_model <- ranger(
+      formula = eq,
+      data = df_train,
+      num.trees = ntrees,
+      mtry = mtry,
+      seed = seed,
+      importance = 'impurity'          # 'permutation'
+   )
+
+   df_all <- rbind(df_train %>% mutate(df_type = 'train'), 
+                   df_test %>% mutate(df_type = 'test'))
+   rf_result <- data.frame(pred = predict(rf_model, df_all) %>% predictions(),
+                           obs = df_all[, y_varname]) %>% 
+      mutate(res = obs - pred) %>% 
+      cbind(df_all %>% dplyr::select(-all_of(y_varname))) 
+   rf_poll_train <- rf_result[rf_result$df_type=='train', ]
+   rf_poll_test <- rf_result[rf_result$df_type=='test', ]
+   
+   eval_train <- error_matrix(rf_poll_train$obs, rf_poll_train$pred)
+   eval_test <- error_matrix(rf_poll_test$obs, rf_poll_test$pred)
+
+   write.csv(rf_result, 
+             paste0('data/workingData/RF_result_all_', csv_name, '.csv'), 
+             row.names = F)
+   # Variable importance
+   var_importance <- data.frame(var_name = rf_model$variable.importance %>% names, 
+                                vi = rf_model$variable.importance %>% as.numeric())
+   var_importance <- var_importance[with(var_importance, order(-vi)), ]
+   
+   
+   write.csv(var_importance, paste0('data/workingData/RF_vi_', csv_name, '.csv'), 
+             row.names = F)
+   return(list(rf_result=rf_result, eval_train=eval_train, eval_test=eval_test))
+   
+   
+}
