10aa141975b449f2fcb88c55b5b57b9bcca8fc18
Youchen Shen <co822ee@gmail.com>
2021-01-22 15:20:21 +0100

    calibrate gwr bandwidth (adaptive) using nngb points

--- scr/fun_gwr.R
+++ scr/fun_gwr.R
@@ -1,5 +1,5 @@
 gwr <- function(train_data, test_data, eu_bnd, cellsize, slr_csvname, local_crs){
-
+   
    sp_train <- sp::SpatialPointsDataFrame(data = train_data,
                                           coords = cbind(train_data$Xcoord, train_data$Ycoord),
                                           proj4string = local_crs)
@@ -20,16 +20,25 @@ gwr <- function(train_data, test_data, eu_bnd, cellsize, slr_csvname, local_crs)
    plot(eu_bnd[1], pch=16, col='firebrick',add=TRUE)
    DM <- gw.dist(dp.locat=coordinates(sp_train),
                  rp.locat=coordinates(grd2))
-   # Calibrate bandwidth using CV
+   DM_1 <- gw.dist(dp.locat=coordinates(sp_train),
+                   rp.locat=coordinates(sp_train))
+   
+   
    # Build GWR
    slr <- read.csv(paste0("data/workingData/SLR_summary_model_", slr_csvname, '.csv'))
    eq <- as.formula(paste0('NO2_2010~',  paste(slr$variables[-1], collapse = "+")))
    summary(lm(eq, data=train_data))
+   # Calibrate bandwidth using CV
+   nngb <- bw.gwr(eq, data=sp_train, approach = "CV",
+                  kernel = "exponential", adaptive = T, dMat = DM_1)
    gwr.res.t <- gwr.basic(eq,
                           data=sp_train, 
-                          regression.points=grd2, bw=50000000, 
+                          regression.points=grd2, 
+                          adaptive = T,
+                          bw=nngb, 
                           dMat=DM,
                           kernel='exponential')
+   
    gwr.res.t
 }
 
