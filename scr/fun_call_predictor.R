275b16fede85712cad5aa4f72790d5e7c561080f
Youchen Shen <co822ee@gmail.com>
2021-01-22 11:58:08 +0100

    comment out unused code (not sure)

--- scr/fun_call_predictor.R
+++ scr/fun_call_predictor.R
@@ -1,13 +1,13 @@
-pred <- list(c('alt_t', 'x_trun', 'y_trun', 'pop2011'),     #'alt10_enh','Xcoord', 'Ycoord', 'XY'
-             'clc10',
-             'clc14',
-             'clc3',
-             'clc5',
-             'clc7',
-             'MAJRDS_EU',
-             'ROADS_EU',
-             c('no2_10MACC', 'PM25_10_MACC','no2_10SAT', 'PM25_10_SAT', 'DEHM_NO2_2010'),
-             'RES')
+# pred <- list(c('alt_t', 'x_trun', 'y_trun', 'pop2011'),     #'alt10_enh','Xcoord', 'Ycoord', 'XY'
+#              'clc10',
+#              'clc14',
+#              'clc3',
+#              'clc5',
+#              'clc7',
+#              'MAJRDS_EU',
+#              'ROADS_EU',
+#              c('no2_10MACC', 'PM25_10_MACC','no2_10SAT', 'PM25_10_SAT', 'DEHM_NO2_2010'),
+#              'RES')
 pred_c <- c(c('alt_t', 'x_trun', 'y_trun', 'pop2011'),
             'clc10',
             'clc14',
