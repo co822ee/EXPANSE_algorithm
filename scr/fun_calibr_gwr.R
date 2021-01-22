a4a61dbf6c9d761b2f870bf53ad5faf4bb9cf2be
Youchen Shen <co822ee@gmail.com>
2021-01-22 16:03:19 +0100

    calibrate gwr bandwidth

---
+++ scr/fun_calibr_gwr.R
@@ -0,0 +1,13 @@
+calibr_gwr <- function(sp_train, slr_csvname){
+   slr <- read.csv(paste0("data/workingData/SLR_summary_model_", slr_csvname, '.csv'))
+   eq <- as.formula(paste0('NO2_2010~',  paste(slr$variables[-1], collapse = "+")))
+   
+   # Calibrate bandwidth using CV
+   DM_1 <- gw.dist(dp.locat=coordinates(sp_train),
+                   rp.locat=coordinates(sp_train))
+   nngb <- bw.gwr(eq, data=sp_train, approach = "CV",
+                  kernel = "exponential", adaptive = T, dMat = DM_1)
+   print(paste0("output data/workingData/GWR_nngb_", slr_csvname, ".txt"))
+   write.table(nngb, paste0("data/workingData/GWR_nngb_", slr_csvname, ".txt"))
+   nngb
+}
