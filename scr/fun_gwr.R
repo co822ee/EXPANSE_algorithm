850ddf1811f4b035ff4e7b57e274689ab664ea07
Youchen Shen <co822ee@gmail.com>
2021-01-25 17:45:36 +0100

    add distance matrix as an input

--- scr/fun_gwr.R
+++ scr/fun_gwr.R
@@ -1,4 +1,4 @@
-gwr <- function(sp_train, grd2, nngb, slr_csvname){
+gwr <- function(sp_train, grd2, dm=DM, nngb, slr_csvname){
    # Build GWR
    slr <- read.csv(paste0("data/workingData/SLR_summary_model_", slr_csvname, '.csv'))
    eq <- as.formula(paste0('NO2_2010~',  paste(slr$variables[-1], collapse = "+")))
@@ -9,7 +9,7 @@ gwr <- function(sp_train, grd2, nngb, slr_csvname){
                           regression.points=grd2, 
                           adaptive = T,
                           bw=nngb, 
-                          dMat=DM,
+                          dMat=dm,
                           kernel='exponential')
    
    gwr.res.t
