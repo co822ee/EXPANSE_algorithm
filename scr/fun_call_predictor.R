10902071f35c73cdc27927f8754a7e1d83ba7e65
Youchen Shen <co822ee@gmail.com>
2021-02-09 14:48:05 +0100

    exclude year and x and y (because it does not make sense for GWR) (but probably need to do the modelling for SLR, GWR, RF separately, because SLR and RF might need x and y)

--- scr/fun_call_predictor.R
+++ scr/fun_call_predictor.R
@@ -8,7 +8,7 @@
 #              'ROADS_EU',
 #              c('no2_10MACC', 'PM25_10_MACC','no2_10SAT', 'PM25_10_SAT', 'DEHM_NO2_2010'),
 #              'RES')
-pred_c <- c(c('alt_t', 'x_trun', 'y_trun', 'pop2011'),
+pred_c <- c(c('alt_t', 'pop2011'),  #'x_trun', 'y_trun',
             'clc10',
             'clc14',
             'clc3',
@@ -17,8 +17,7 @@ pred_c <- c(c('alt_t', 'x_trun', 'y_trun', 'pop2011'),
             'MAJRDS_EU',
             'ROADS_EU',
             c('no2_10MACC', 'PM25_10_MACC','no2_10SAT', 'PM25_10_SAT', 'DEHM_NO2_2010'),
-            'RES',
-            'year')
-neg_pred <- c('alt10_enh', 'clc14', 'clc7')
+            'RES') #year
+neg_pred <- c('alt10_enh', 'clc14', 'clc7') #'y_trun'
 station_info <- c('airid', 'Year', 'country_is', 'type_of_st', 'Xcoord', 'Ycoord', 
                   'REGION', 'strata_run5', 'country_full')
