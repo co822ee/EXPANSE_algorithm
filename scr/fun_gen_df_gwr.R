c9fa0649f179b548d25fc08d06fdf13487c93d24
Youchen Shen <co822ee@gmail.com>
2021-02-09 11:08:05 +0100

    specify raster library for extract()

--- scr/fun_gen_df_gwr.R
+++ scr/fun_gen_df_gwr.R
@@ -1,6 +1,6 @@
 gen_df_gwr <- function(coef_stack, sp_p, df_p){
    # extract coefficient values for each point
-   coef_df <- lapply(seq_along(sp_p), function(loc_i) extract(coef_stack, sp_p[loc_i,]))
+   coef_df <- lapply(seq_along(sp_p), function(loc_i) raster::extract(coef_stack, sp_p[loc_i,]))
    coef_df <- Reduce(rbind, coef_df)
    
    predictor_test <- cbind(Intercept=1, df_p %>% dplyr::select(colnames(coef_df)[-1]))
