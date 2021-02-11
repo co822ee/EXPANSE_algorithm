8c6e0c5cf12448f3e6eda2bfe4071ce89d641a4f
Youchen Shen <co822ee@gmail.com>
2021-02-11 12:35:59 +0100

    replace df with data_df

--- scr/fun_gen_pred_df.R
+++ scr/fun_gen_pred_df.R
@@ -1,11 +1,11 @@
-gen_pred_df <- function(model, df, obs_varname){
+gen_pred_df <- function(model, data_df, obs_varname){
    # model is the final linear regression model 
-   # df is the df in which predictor values and observations are stored (as well as the station info)
+   # data_df is the data_df in which predictor values and observations are stored (as well as the station info)
    # This function return the observations (obs) and predictions (pred) as well as station info.
-   poll <- data.frame(slr = predict(model, df) %>% as.numeric(),
-                      obs = df[, obs_varname]) %>% 
+   poll <- data.frame(slr = predict(model, data_df) %>% as.numeric(),
+                      obs = data_df[, obs_varname]) %>% 
       mutate(res = obs - slr) %>% 
-      cbind(df %>% dplyr::select(-all_of(obs_varname)))
+      cbind(data_df %>% dplyr::select(-all_of(obs_varname)))
    
    return(poll)
 }
