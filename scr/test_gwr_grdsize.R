954d107f7378bd523f1283264090b6408f9be1de
Youchen Shen <co822ee@gmail.com>
2021-03-11 12:16:48 +0100

    clean the code and visualize the influence of grid cell size (it is just a tool for reflecting the local information defined by the bandwidth)

--- scr/test_gwr_grdsize.R
+++ scr/test_gwr_grdsize.R
@@ -1,51 +1,10 @@
-# This script is intended to test GWR models with different parameter settings
-# Bandwidth (adaptive or not)
-# kernel type (just Gaussian and exponential)
-# regression grid cellsize
-library(dplyr)
-library(raster)
-library(sf)
-library(car)  # for running slr
-library(GWmodel)  #gwr
-library(viridis)  #palette for raster
-library(ranger) # Random forests
-library(caret)  #data partition
-library(tmap)
-library(splitstackshape)   #stratified function in this library is better than createDataPartition in library caret
-library(splitTools)
-library(APMtools)
-library(lme4) # linear mixed effect models
-library(CAST) # For dividing training and test data (CreateSpacetimeFolds)
-library(performance) #extract model performance matrix for lme
-
-seed <- 123
-local_crs <- CRS("+init=EPSG:3035")
-
-eu_bnd <- st_read("../expanse_shp/eu_expanse2.shp")
-## Read in data (elapse NO2 2010 with climate zones included)
-elapse_no2 <- read.csv("../EXPANSE_predictor/data/processed/no2_2010_elapse_climate.csv",
-                       encoding = "utf-8")
-## Read in data (airbase observations 1990s-2012)
-no2 <- read.csv("../EXPANSE_APM/data/processed/ab_v8_yr_no2.csv")
-# rename data
-elapse_no2 <- rename(elapse_no2, station_european_code=ï..Station)
-# reduce airbase data
-no2 <- no2 %>% rename(year=statistics_year, obs=statistic_value)
-## subset stations that are included in the elapse (cause at this stage, we don't have the predictor maps...)
-no2_e <- no2 %>% filter(no2$station_european_code%in%unique(elapse_no2$station_european_code))
-no2_e_all <- left_join(no2_e, elapse_no2, by="station_european_code")
-source("../EXPANSE_APM/src/fun_eda_spatial_distribution_annualobs_laea.R")
-annual_spatial_dist(poll_conc = no2_e_all, eu_bnd = eu_bnd, 
-                    folder_subnote = 'elapse_ab')
-## subset samples (for multiple years or each year)
-subset_df_yrs <- function(obs_df, yr_target){
-   no2_e_sub <- obs_df %>% filter(year%in%yr_target)
-   no2_e_sub
-}
-#o# multiple years
+# This script is intended to test GWR models with different settings of
+# regression grid cellsize.
+source("scr/fun_call_lib.R")
+source("scr/fun_read_data.R")
 
 #---------Test the bandwidth----------
-global_vars <- "no2_10MACC"
+# global_vars <- "no2_10MACC"
 
 regression_grd_cellsize <- c(10, 20, 50, 80, 100, 200, 500, 600, 1000, 1500, 2000)   #km
 kernels <- c('exponential')
@@ -53,6 +12,12 @@ year_target <- 2008
 
 comb <- expand.grid(regression_grd_cellsize=regression_grd_cellsize, kernel_type=kernels) %>% 
    mutate(csv_name = paste0('testGWR_', regression_grd_cellsize, '_', kernel_type, '_', year_target))
+kernel_type <- comb$kernel_type %>% as.character()
+reg_grdsize <- comb$regression_grd_cellsize*1000
+csv_names <- comb$csv_name
+# csv_names <- paste0('testGWR_', regression_grd_cellsize, "_", year_target)
+years <- as.list(rep(year_target, length(csv_names)))
+
 # perf_matrix <- data.frame(RMSE=0, RRMSE=0, IQR=0, rIQR=0, MAE=0, rMAE=0, rsq=0, explained_var=0,
 #                           csv_name="0", datatype="0")
 library(doParallel)
@@ -60,87 +25,36 @@ library(foreach)
 cl <- parallel::makeCluster(5)
 doParallel::registerDoParallel(cl)
 foreach(i=seq_len(nrow(comb))) %dopar% {
-   library(dplyr)
-   library(raster)
-   library(sf)
-   library(car)  # for running slr
-   library(GWmodel)  #gwr
-   library(viridis)  #palette for raster
-   library(ranger) # Random forests
-   library(caret)  #data partition
-   library(splitstackshape)   #stratified function in this library is better than createDataPartition in library caret
-   library(splitTools)
-   library(APMtools)
-   library(lme4) # linear mixed effect models
-   library(CAST) # For dividing training and test data (CreateSpacetimeFolds)
-   library(performance) #extract model performance matrix for lme
-   library(tmap)
-   seed <- 123
-   local_crs <- CRS("+init=EPSG:3035")
-   
-   eu_bnd <- st_read("../expanse_shp/eu_expanse2.shp")
-   ## Read in data (elapse NO2 2010 with climate zones included)
-   elapse_no2 <- read.csv("../EXPANSE_predictor/data/processed/no2_2010_elapse_climate.csv",
-                          encoding = "utf-8")
-   ## Read in data (airbase observations 1990s-2012)
-   no2 <- read.csv("../EXPANSE_APM/data/processed/ab_v8_yr_no2.csv")
-   # rename data
-   elapse_no2 <- rename(elapse_no2, station_european_code=ï..Station)
-   # reduce airbase data
-   no2 <- no2 %>% rename(year=statistics_year, obs=statistic_value)
-   ## subset stations that are included in the elapse (cause at this stage, we don't have the predictor maps...)
-   no2_e <- no2 %>% filter(no2$station_european_code%in%unique(elapse_no2$station_european_code))
-   no2_e_all <- left_join(no2_e, elapse_no2, by="station_european_code")
-   
-   ## subset samples (for multiple years or each year)
-   subset_df_yrs <- function(obs_df, yr_target){
-      no2_e_sub <- obs_df %>% filter(year%in%yr_target)
-      no2_e_sub
-   }
-   #o# multiple years
-   # Mixed GWR
-   global_vars <- "no2_10MACC"
-   
-   #---------Test the bandwidth----------
-   # Test the kernel function:
-   regression_grd_cellsize <- c(10, 20, 50, 80, 100, 200, 500, 600, 1000, 1500, 2000)   #km
-   kernels <- c('exponential')
-   year_target <- 2008
-   comb <- expand.grid(regression_grd_cellsize=regression_grd_cellsize, kernel_type=kernels) %>% 
-      mutate(csv_name = paste0('testGWR_', regression_grd_cellsize, '_', kernel_type, '_', year_target))
-   kernel_type <- comb$kernel_type %>% as.character()
-   reg_grdsize <- comb$regression_grd_cellsize*1000
-   csv_names <- comb$csv_name
-   # csv_names <- paste0('testGWR_', regression_grd_cellsize, "_", year_target)
-   years <- as.list(rep(year_target, length(csv_names)))
-   
-   
+   source("scr/fun_call_lib.R")
    csv_name <- csv_names[i]
    print(csv_name)
-   no2_e_09_11 <- subset_df_yrs(no2_e_all, years[[i]])
-   data_all <- no2_e_09_11
+   no2_e_target <- subset_df_yrs(no2_e_all, years[[i]])
+   data_all <- no2_e_target
    
    #f# stratified by station types, climate zones and/or years
    set.seed(seed)
    data_all$index <- 1:nrow(data_all)
    train_sub <- stratified(data_all, c('type_of_st', 'zoneID'), 0.8)
    test_sub <- data_all[-train_sub$index, ]
+   data_all <- rbind(train_sub, test_sub)
    
    all(unique(train_sub$station_european_code)%in%unique(test_sub$station_european_code))
    all_sub <- rbind(train_sub %>% mutate(df_type='train'), 
                     test_sub %>% mutate(df_type='test'))
    
-   all_sp <- SpatialPointsDataFrame(coords = cbind(all_sub$Xcoord, all_sub$Ycoord),
-                                    all_sub, proj4string = local_crs)
-   
-   
-   sum(train_sub$type_of_st=="Background"&train_sub$climate_zone==1)/nrow(train_sub)
-   sum(test_sub$type_of_st=="Background"&test_sub$climate_zone==1)/nrow(test_sub)
+   # all_sp <- SpatialPointsDataFrame(coords = cbind(all_sub$Xcoord, all_sub$Ycoord),
+   #                                  all_sub, proj4string = local_crs)
+   sum(train_sub$type_of_st=="Background"&train_sub$zoneID==1)/nrow(train_sub)
+   sum(test_sub$type_of_st=="Background"&test_sub$zoneID==1)/nrow(test_sub)
    sum(train_sub$type_of_st=="Background")/nrow(train_sub)
    sum(test_sub$type_of_st=="Background")/nrow(test_sub)
+   sum(train_sub$type_of_st=="Industrial")/nrow(train_sub)
+   sum(test_sub$type_of_st=="Industrial")/nrow(test_sub)
+   sum(train_sub$type_of_st=="Traffic")/nrow(train_sub)
+   sum(test_sub$type_of_st=="Traffic")/nrow(test_sub)
    # Check whether every station serve as the same type of data over years
    any(unique(train_sub$station_european_code)%in%unique(test_sub$station_european_code))
-   # --> yes indeed the stations are not the same in training and test data
+   # --> FALSE: yes indeed the stations are not the same in training and test data
    #TODO we need to look at the groups separately or in combined?
    #f# SLR: select predictors
    source("scr/fun_call_predictor.R")
@@ -150,7 +64,6 @@ foreach(i=seq_len(nrow(comb))) %dopar% {
    test_sub <- proc_in_data(test_sub, neg_pred)
    #------------------Above code is needed for all algorithms----------------------
    #---------#f# SLR: train SLR -----------
-   
    slr <- read.csv(paste0("data/workingData/SLR_summary_model_run2_", years[[i]],".csv"))
    slr_poll <- read.csv(paste0('data/workingData/SLR_result_all_run2_', years[[i]],".csv"))
    eq <- as.formula(paste0('obs~',  paste(slr$variables[-1], collapse = "+")))
@@ -213,8 +126,19 @@ foreach(i=seq_len(nrow(comb))) %dopar% {
                                       dMat=DM,
                                       kernel=kernel_type[i]),
                             error=function(e) T)
+   ncol(DM)
+   grd_i=13
+   gw.weight(DM[,grd_i], nngb, kernel_type[i], T) %>% hist  #grd_i is the regression point index.
    
    
+   if(!file.exists(paste0("data/workingData/GWR_max_w_", 
+                          kernel_type[i], "_", years[[i]], ".txt"))){
+      max_w <- lapply(1:ncol(DM), function(grd_i) gw.weight(DM[,grd_i], nngb, kernel_type[i], T) %>% max)
+      write.table(unlist(max_w), paste0("data/workingData/GWR_max_w_", 
+                                reg_grdsize[i], "_", years[[i]], ".txt"), row.names = F)
+   }
+   
+   hist(unlist(max_w))
    # error: inv(): matrix seems singular
    if(!(typeof(gwr_model_ad)=='logical')){
       #f# GWR: perform cross-validation
@@ -270,6 +194,9 @@ perfm$kernel <- c(lapply(perfm$csv_name[1:nrow(perfm)], function(f) strsplit(f, 
 ggplot(perfm)+
    geom_line(aes(x=reg_grdsize, y=rsq, col=df_type))+
    geom_point(aes(x=reg_grdsize, y=rsq, col=df_type))+
-   labs(title=paste0(unique(perfm$year), " (", unique(perfm$kernel), ")"))
+   labs(title=paste0(unique(perfm$year), " (", unique(perfm$kernel), ")"),
+        x='grid cell size of regression coefficients (km)',
+        y='R squared',
+        col='data type')
 
 read.csv(paste0("data/workingData/SLR_summary_model_run1_train_break_noxy", 2009,".csv"))
---
+++ scr/test_gwr_grdsize_vis.R
@@ -0,0 +1,54 @@
+source("scr/fun_call_lib.R")
+source("scr/fun_read_data.R")
+source("scr/fun_setupt_gwr.R")
+# train_expanse <- stratified(no2[no2$year==2010,], c('type_of_st', 'zoneID'), 0.8)
+# test_expanse <- no2[no2$year==2010,][-train_expanse$index, ]
+setup <- setup_gwr(train_expanse, eu_bnd, 
+                   cellsize = reg_grdsize[i], local_crs = local_crs)
+setup_test <- setup_gwr(train_sub, eu_bnd, 
+                     cellsize = reg_grdsize[8], local_crs = local_crs) #600km
+dm_test <- setup_test[[3]]
+grd_test <- setup_test[[2]]
+coordinates(grd_test)[1,]
+
+setup_test2 <- setup_gwr(train_sub, eu_bnd, 
+                        cellsize = reg_grdsize[6], local_crs = local_crs) #200km
+dm_test2 <- setup_test2[[3]]
+grd_test2 <- setup_test2[[2]]
+coordinates(grd_test2)[1,]
+crs(grd_test) <- crs(eu_bnd)
+crs(grd_test2) <- crs(eu_bnd)
+
+
+# Visualize overall
+plot(grd_test)
+plot(grd_test2, add=T, col='blue')
+# points(coordinates(grd_test), pch=5)
+text(coordinates(grd_test)[,1], coordinates(grd_test)[,2], seq_along(grd_test))
+text(coordinates(grd_test2)[,1], coordinates(grd_test2)[,2], seq_along(grd_test2), cex=0.5)
+# points(coordinates(grd_test2), col='blue', pch=16)
+plot(eu_bnd[1], pch=16, col='transparent', border='grey', lambda=0.2, add=TRUE)
+text(coordinates(grd_test)[,1], coordinates(grd_test)[,2], seq_along(grd_test),
+     col='dark green')
+plot(grd_test, add=T, col='dark green', lty=2)
+
+#38 for grd_test
+#292 for grd_test2
+grd_i <- 38
+grd_i2 <- 292
+coordinates(grd_test)[grd_i,]
+coordinates(grd_test2)[grd_i2,]
+gw.weight(dm_test[,grd_i], nngb, kernel_type[i], T) %>% hist  #grd_i is the regression point index.
+gw.weight(dm_test2[,grd_i2], nngb, kernel_type[i], T) %>% hist  #grd_i is the regression point index.
+hist(gw.weight(dm_test[,grd_i], nngb, kernel_type[i], T)-gw.weight(dm_test2[,grd_i2], nngb, kernel_type[i], T))
+boxplot(gw.weight(dm_test[,grd_i], nngb, kernel_type[i], T)-gw.weight(dm_test2[,grd_i2], nngb, kernel_type[i], T))
+
+# Visualize (France) # not working
+# plot(grd_test)
+# plot(eu_bnd[which(eu_bnd[1]$CNTR_ID=="FR"), 1], pch=16, col='transparent', border='grey', main=NULL)
+# plot(eu_bnd[eu_bnd$CNTR_ID=="FR",], pch=16, col='transparent', border='grey', main=NULL)
+# plot(grd_test2, add=T, col='blue')
+
+DM_test <- setup_test[[3]]
+max_w <- lapply(1:ncol(DM_expanse), function(grd_i) gw.weight(DM_expanse[,grd_i], nngb, kernel_type[i], T) %>% max)
+hist(unlist(max_w))
