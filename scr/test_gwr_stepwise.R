870686bc251176c16c051173faa29ee0d43d9ea5
Youchen Shen <co822ee@gmail.com>
2021-03-11 12:15:24 +0100

    check the collinearity problem/effect --> none

--- scr/test_gwr_stepwise.R
+++ scr/test_gwr_stepwise.R
@@ -26,7 +26,7 @@ set.seed(seed)
 data_all$index <- 1:nrow(data_all)
 train_sub <- stratified(data_all, c('type_of_st', 'zoneID'), 0.8)
 test_sub <- data_all[-train_sub$index, ]
-
+data_all <- rbind(train_sub, test_sub)
 # --> yes indeed the stations are not the same in training and test data
 #TODO we need to look at the groups separately or in combined?
 #f# SLR: select predictors
@@ -235,6 +235,78 @@ output
 if(any(diff(output$increR2)<0.01)) output <- output[-(which(diff(output$increR2)<0.01)+1),]
 write.table(output, paste0("data/workingData/stepGWR_", 
                            year_target, ".txt"), row.names = F)
+
+#----------VIF evaluation----------
+output <- read.table(paste0("data/workingData/stepGWR_", 
+                            year_target, ".txt"), header = T)
+eq <- as.formula(paste0('obs~', paste(output$variables, collapse = "+")))
+lm_model <- lm(eq, train_sub)
+library(car)
+vif(lm_model)<3
+# All are below 3 (No collinearity in global regression model)
+# Check local collinearity for basic GWR
+source("scr/fun_setupt_gwr.R")
+setup <- setup_gwr(train_sub, eu_bnd, 
+                   cellsize = reg_grdsize, local_crs = local_crs)
+sp_train <- setup[[1]]
+grd <- setup[[2]]
+DM <- setup[[3]]
+DM_1 <- gw.dist(dp.locat=coordinates(sp_train),
+                rp.locat=coordinates(sp_train))
+nngb <- bw.gwr(eq, data=sp_train, approach = "CV", kernel = kernel_type,
+               adaptive = T, dMat = DM_1)
+gwr_collin <- gwr.collin.diagno(eq, sp_train, kernel = kernel_type, bw=nngb,
+                                adaptive = T, dMat = DM_1)
+which(gwr_collin$VIF>=3, arr.ind = T)
+which(gwr_collin$VIF>=5, arr.ind = T)
+which(gwr_collin$local_CN>=3)
+which(gwr_collin$local_CN>=10)
+which(gwr_collin$VIF>=10, arr.ind = T)
+hist(gwr_collin$local_CN)
+length(which(gwr_collin$local_CN>=20))/length(gwr_collin$local_CN)
+# For some observation points, the VIFs of some predictors exceed 3
+# It is not ideal to remove the predictors, when only a local collinearity effect is present.
+# We can do nothing, but such collinearity effects can cause a loss of precision and power in the coefficient estimates.
+# Also, studies have shown that GWR may find patterns in the coefficients where no spatial patterns are actually present (ref).
+# Therefore, here we try to use locally-compensated GW regression (with ridge term)
+# Biased local estimations are only used at locations where collinearity is likely to be an issue
+nngb_lcr <- bw.gwr.lcr(eq, sp_train, kernel = kernel_type,
+                       lambda.adjust = T, cn.thresh = 20,  #commonly set between 20 and 30 # lambda=1, 
+                       adaptive = T, dMat = DM_1)
+gwr_lcr <- gwr.lcr(eq, sp_train, regression.points=grd,
+                   lambda.adjust = T, cn.thresh = 20,   # lambda=1, 
+                   adaptive = T, bw=nngb_lcr, dMat=DM, kernel=kernel_type)
+summary(gwr_lcr$SDF$Local_CN)
+gwr_model <- gwr.basic(eq, sp_train, regression.points=grd,
+                       adaptive = T, bw=nngb, dMat=DM, kernel=kernel_type)
+source('scr/fun_plot_gwr_coef.R')
+plot_gwr_coef(i, gwr_lcr, paste0(csv_name, "_lcr20"), n_row = 3, n_col = 3, eu_bnd = eu_bnd)
+plot_gwr_coef(i, gwr_model, csv_name, n_row = 2, n_col = 3, eu_bnd = eu_bnd)
+# compare the two result
+# plot(gwr_model$SDF$Intercept, gwr_lcr$SDF$Intercept)
+# plot(gwr_model$SDF$ROADS_EU_5p, gwr_lcr$SDF$ROADS_EU_5p)
+windows()
+par(mfrow=c(2,3))
+for(plot_i in seq_along(names(gwr_model$SDF))){
+   plot(gwr_model$SDF[[plot_i]], gwr_lcr$SDF[[plot_i]], main=names(raster(gwr_model$SDF[plot_i])))
+   abline(a=0, b=1)
+}
+gwr_plot <- vector(mode = "list", length = ncol(gwr_model$SDF))
+for(plot_i in seq_along(names(gwr_model$SDF))){ #seq_along(names(gwr_model$SDF))
+   gwr_plot[[plot_i]] <- tm_shape((raster(gwr_lcr$SDF[plot_i])-raster(gwr_model$SDF[plot_i]))/raster(gwr_model$SDF[plot_i])*100)+
+      tm_raster(palette = viridis(5), style = "cont", title = '')+
+      tm_shape(eu_bnd) +
+      tm_borders(col='black')+
+      tm_layout(legend.title.size = 1, legend.text.size = 0.8, 
+                legend.text.color = 'yellow', 
+                title = names(raster(gwr_model$SDF[plot_i])),
+                title.color = 'black')
+}
+gwr_plot$nrow <- 2
+gwr_plot$ncol <- 3
+mergeMap <- do.call(tmap_arrange, gwr_plot)
+tmap_save(mergeMap, filename = paste0('graph/gwr_coef/', csv_name, "_gwr-lcr.tiff"), 
+          dpi=100, height=10, width=10, units='in')
 #---------evaluate output----------
 output <- read.table(paste0("data/workingData/stepGWR_", 
                             year_target, ".txt"), header = T)
