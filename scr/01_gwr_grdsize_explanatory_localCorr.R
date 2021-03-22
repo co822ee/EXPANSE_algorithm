772ef0d86e1881aacc8f6058049e34188a43818b
Youchen Shen <co822ee@gmail.com>
2021-03-22 20:33:56 +0100

    to complete

--- scr/01_gwr_grdsize_explanatory_localCorr.R
+++ scr/01_gwr_grdsize_explanatory_localCorr.R
@@ -1,13 +1,29 @@
 # This script uses gwr local statistics to demonstrate the local variations (especially for no2_10MACC)
-data("DubVoter")
 library(gridExtra)
 library(RColorBrewer)
 mypalette.1 <- viridis(5)
-gw.ss.bx <- gwss(Dub.voter, vars = c("GenEl2004", "LARent", "Unempl"),
-                 kernel = "boxcar", adaptive = TRUE, bw = 48, quantile = TRUE)
-gw.ss.bs <- gwss(Dub.voter,vars = c("GenEl2004", "LARent", "Unempl"),
-                 kernel = "bisquare", adaptive = TRUE, bw = 48)
+source("scr/fun_call_lib.R")
+source("scr/fun_create_fold.R")
+source("scr/fun_read_data.R")
+source("scr/fun_call_predictor.R")
+years <- 2010
+i=1
+fold_i=1
+no2_e_09_11 <- subset_df_yrs(no2_e_all, years[[i]])
+# data_all <- no2_e_09_11
+print(paste0("year: ", unique(no2_e_09_11$year)))
+data_all <- create_fold(no2_e_09_11, seed)
 
+test_data <- data_all[data_all$nfold==fold_i,]
+train_data <- data_all[-test_data$index, ] #data_all$index starts from 1 to the length.
+
+local_crs <- CRS("+init=EPSG:3035")
+sp_train <- sp::SpatialPointsDataFrame(data = train_data,
+                                       coords = cbind(train_data$Xcoord, train_data$Ycoord),
+                                       proj4string = local_crs)
+sp_test <- sp::SpatialPointsDataFrame(data = test_data,
+                                      coords = cbind(test_data$Xcoord, test_data$Ycoord),
+                                      proj4string = local_crs)
 gw.ss.bx2 <- gwss(sp_train, vars = c(slr$variables[-1], 'obs'), kernel = 'gaussian', 
                  adaptive = T, bw=nngb)
 gw.ss.bx3 <- gwss(sp_train, vars = c(slr$variables[-1], 'obs'), kernel = 'gaussian', 
@@ -58,3 +74,12 @@ gw.ss.bs <- gwss(sp_train, vars = slr$variables[-1], kernel = kernel_type[i],
 
 gw.ss.bs2 <- gwss(sp_train, vars = slr$variables[-1], kernel = kernel_type[i], 
                   adaptive = F, bw=bandwidth_calibr)
+
+
+
+# Demo code
+# data("DubVoter")
+# gw.ss.bx <- gwss(Dub.voter, vars = c("GenEl2004", "LARent", "Unempl"),
+#                  kernel = "boxcar", adaptive = TRUE, bw = 48, quantile = TRUE)
+# gw.ss.bs <- gwss(Dub.voter,vars = c("GenEl2004", "LARent", "Unempl"),
+#                  kernel = "bisquare", adaptive = TRUE, bw = 48)
