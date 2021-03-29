06b8ac8d034cefdce30fe67b914a0a8ab3369779
Youchen Shen (mac uu) <co822ee@gmail.com>
2021-03-29 13:19:13 +0200

    change the code for different xcoord and ycoord names; comment the altitude transofrmation cause we don't have altitude as a predictor now

--- scr/fun_slr_proc_in_data.R
+++ scr/fun_slr_proc_in_data.R
@@ -1,9 +1,12 @@
-proc_in_data <- function(in_data, neg_pred){
+proc_in_data <- function(in_data, neg_pred, xcoord="Xcoord", ycoord="Ycoord"){
    # truncate x and y
-   in_data <- in_data %>% mutate(x_trun = (Xcoord-min(Xcoord))/(max(Xcoord)-min(Xcoord)),
-                                 y_trun = (Ycoord-min(Ycoord))/(max(Ycoord)-min(Ycoord)))
+   in_data$x_trun <- in_data[,xcoord]-min(in_data[,xcoord])/(max(in_data[,xcoord])-min(in_data[,xcoord]))
+   
+   in_data$y_trun <- in_data[,ycoord]-min(in_data[,ycoord])/(max(in_data[,ycoord])-min(in_data[,ycoord]))
+   # in_data <- in_data %>% mutate(x_trun = (Xcoord-min(Xcoord))/(max(Xcoord)-min(Xcoord)),
+   #                               y_trun = (Ycoord-min(Ycoord))/(max(Ycoord)-min(Ycoord)))
    # transform altitude
-   in_data <- in_data %>% mutate(alt_t = sqrt((alt10_enh-min(alt10_enh))/max(alt10_enh-min(alt10_enh))))
+   # in_data <- in_data %>% mutate(alt_t = sqrt((alt10_enh-min(alt10_enh))/max(alt10_enh-min(alt10_enh))))
    # add year as a grouping factor
    in_data$year <- as.factor(in_data$year)
    # change direction of effect
