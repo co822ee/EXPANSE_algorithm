6857a2613335b3535e9e30c75563439182bdd4ce
Youchen Shen (mac uu) <co822ee@gmail.com>
2021-03-29 17:06:06 +0200

    modify code for the new EXPANSE data

---
+++ o_01_output_prediction_test_cv.R
@@ -0,0 +1,31 @@
+source("scr/fun_call_lib.R")
+source("scr/o_00_00_read_data.R")
+# Multiple single years
+csv_names <- paste0('o_', c(2005:2012))   #2008:2012
+years <- as.list(c(2005:2012))
+# year_i=1
+csv_names
+nfold=5
+
+write_output_5csv <- function(year_i){
+   # paste0("GWR_result_all_", csv_names[year_i], "_fold_", seq(1,nfold), ".csv")
+   # list.files("data/workingData/", paste0("GWR_result_all_", csv_names[year_i], "_fold"))
+   slr <- lapply(paste0("data/workingData/SLR_result_all_", csv_names[year_i], "_fold_", seq(1,nfold), ".csv"), 
+                 read.csv)
+   gwr <- lapply(paste0("data/workingData/GWR_result_all_", csv_names[year_i], "_fold_", seq(1,nfold), ".csv"), 
+                 read.csv)
+   rf <- lapply(paste0("data/workingData/RF_result_all_", csv_names[year_i], "_fold_", seq(1,nfold), ".csv"), 
+                read.csv)
+   slr_test <- lapply(slr, function(df_data) df_data %>% filter(df_type=='test'))
+   gwr_test <- lapply(gwr, function(df_data) df_data %>% filter(df_type=='test'))
+   rf_test <- lapply(rf, function(df_data) df_data %>% filter(df_type=='test'))
+   
+   slr_test <- do.call(rbind, slr_test)
+   gwr_test <- do.call(rbind, gwr_test)
+   rf_test <- do.call(rbind, rf_test)
+   
+   all_test <- cbind(gwr=gwr_test$gwr, rf=rf_test$rf, slr_test)
+   write.csv(all_test, paste0("data/workingData/o_NO2_5cv_", years[[year_i]], ".csv"))
+   
+}
+lapply(seq_along(csv_names), write_output_5csv)
