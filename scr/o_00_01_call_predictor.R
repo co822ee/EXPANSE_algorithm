2c6ae856606185c9ffde992c58a1685c265bf296
Youchen Shen (mac uu) <co822ee@gmail.com>
2021-05-04 12:10:10 +0200

    update the method for extracting predictors

--- scr/o_00_01_call_predictor.R
+++ scr/o_00_01_call_predictor.R
@@ -1,3 +1,7 @@
-# In the previous step, pred_c has been extracted from the pred dataframe.
+# 
+exc_names <- c("sta_code", "component_code", "component_caption", "obs", 
+               "year", "id", "country_name", "sta_type", "area_type", "areaid", 
+               "index", "nfold", "xcoord", "ycoord")
+pred_c <- names(data_all1)[!names(data_all1)%in%exc_names]
 neg_pred <- pred_c[grepl("nat|ugr", pred_c)]
 # Ps. altitude and population and SAT are not included
