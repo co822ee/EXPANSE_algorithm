21effb033a817cb4b2590bd357dec5981d080f82
Youchen Shen (mac uu) <co822ee@gmail.com>
2021-05-04 12:10:30 +0200

    change hard coding

--- scr/o_00_01_split_data.R
+++ scr/o_00_01_split_data.R
@@ -8,7 +8,7 @@ no2_e_09_11 <- subset_df_yrs(no2_e_all, years[[yr_i]])
 source("scr/fun_create_fold.R")
 data_all1 <- create_fold(no2_e_09_11, seed, strt_group=c("sta_type", "zoneID"), 
                          multiyr_vargroup = "sta_code", 
-                         nfold = 5)
+                         nfold = nfold)
 
 csv_name_fold <- paste0(csv_name, "_fold_", fold_i)
 test_sub <- data_all1[data_all1$nfold==fold_i,]
