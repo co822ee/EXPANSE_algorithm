631ee808a5b4d022aeb8dee55996e88ae6e5437e
Youchen Shen (mac uu) <co822ee@gmail.com>
2021-09-28 13:07:37 +0200

    move to archive

--- scr/o_00_01_call_predictor.R
+++
@@ -1,7 +0,0 @@
-# 
-exc_names <- c("sta_code", "component_code", "component_caption", "obs", 
-               "year", "id", "country_name", "sta_type", "area_type", "areaid", 
-               "index", "nfold", "xcoord", "ycoord")
-pred_c <- names(data_all1)[!names(data_all1)%in%exc_names]
-neg_pred <- pred_c[grepl("nat|ugr", pred_c)]
-# Ps. altitude and population and SAT are not included
