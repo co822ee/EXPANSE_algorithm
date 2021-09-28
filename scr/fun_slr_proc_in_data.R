c31a9fbe6a19a136b1e5d4c61a05045b3a27d764
Youchen Shen (mac uu) <co822ee@gmail.com>
2021-09-28 13:09:26 +0200

    as factor

--- scr/fun_slr_proc_in_data.R
+++ scr/fun_slr_proc_in_data.R
@@ -1,7 +1,7 @@
 proc_in_data <- function(in_data, neg_pred, xcoord="Xcoord", ycoord="Ycoord"){
    # truncate x and y
    in_data$x_trun <- (in_data[,xcoord]-min(in_data[,xcoord]))/(max(in_data[,xcoord])-min(in_data[,xcoord]))
-   
+
    in_data$y_trun <- (in_data[,ycoord]-min(in_data[,ycoord]))/(max(in_data[,ycoord])-min(in_data[,ycoord]))
    # in_data <- in_data %>% mutate(x_trun = (Xcoord-min(Xcoord))/(max(Xcoord)-min(Xcoord)),
    #                               y_trun = (Ycoord-min(Ycoord))/(max(Ycoord)-min(Ycoord)))
@@ -9,6 +9,8 @@ proc_in_data <- function(in_data, neg_pred, xcoord="Xcoord", ycoord="Ycoord"){
    # in_data <- in_data %>% mutate(alt_t = sqrt((alt10_enh-min(alt10_enh))/max(alt10_enh-min(alt10_enh))))
    # add year as a grouping factor
    in_data$year <- as.factor(in_data$year)
+   in_data$cntr_code <- as.factor(in_data$cntr_code)
+   in_data$zoneID <- as.factor(in_data$zoneID)
    # change direction of effect
    in_data %>% as_tibble() %>% mutate(across(matches(neg_pred), function(x) -x ))
 }
