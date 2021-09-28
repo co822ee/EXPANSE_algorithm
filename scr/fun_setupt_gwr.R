e3d50eb85bcba055013fbbb87b0788c73c5b4fa6
Youchen Shen (mac uu) <co822ee@gmail.com>
2021-09-28 13:09:17 +0200

    minor

--- scr/fun_setupt_gwr.R
+++ scr/fun_setupt_gwr.R
@@ -1,4 +1,5 @@
 setup_gwr <- function(train_data, eu_bnd, cellsize, local_crs, xcoord="Xcoord", ycoord="Ycoord"){
+   ## eu_bnd gives the extent of your study area
    sp_train <- sp::SpatialPointsDataFrame(data = train_data,
                                           coords = cbind(train_data[, xcoord], train_data[, ycoord]),
                                           proj4string = local_crs)
@@ -11,7 +12,7 @@ setup_gwr <- function(train_data, eu_bnd, cellsize, local_crs, xcoord="Xcoord", 
    grd2 <- SpatialGrid(GridTopology(c(xmin,ymin),
                                     c(cellsize,cellsize),
                                     c(floor((xmax-xmin)/cellsize)+2,floor((ymax-ymin)/cellsize)+2)),
-                       proj4string = crs(eu_bnd))
+                       proj4string = local_crs)
    
    # plot(grd2)
    # plot(eu_bnd[1], pch=16, col='firebrick',add=TRUE)
